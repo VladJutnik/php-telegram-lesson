5d089ff77d5c18f245d7648523141f1aca3b4c3e
VladJutnik <v2517v@gmail.com>
2021-10-12 17:12:50 +0300

    Телеграмм Бот на php

---
+++ telegram.php
@@ -0,0 +1,27 @@
+<?php
+
+/* https://api.telegram.org/bot1905085020:AAHYn05zW47lxu3DKsN8RUaotmJ3MTxDaCI/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "";//сюда должен быть ваш токкен помещен
+$chat_id = "";//а сюда номер чата!
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
